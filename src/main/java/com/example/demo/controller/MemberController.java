9f34027ee7933ccff2e4bf875f2c6e4a3f619b52
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-03-25 09:42:26 +0900

    name change

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MemberContorller {
+public class MemberController {
 
     private final MemberService memberService;
 
     @Autowired
-    public MemberContorller(MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
