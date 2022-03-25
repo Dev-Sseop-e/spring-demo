3f8d3fd88b028c15aa514b0b4a515a3683d21eec
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-03-25 17:19:27 +0900

    add: JPA

--- src/main/java/com/example/demo/SpringConfig.java
+++ src/main/java/com/example/demo/SpringConfig.java
@@ -1,21 +1,22 @@
 package com.example.demo;
 
-import com.example.demo.repository.JdbcMemberRepository;
-import com.example.demo.repository.JdbcTemplateMemberRepository;
-import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.repository.*;
 import com.example.demo.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
 
-    public SpringConfig(DataSource dataSource) {
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -27,7 +28,8 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 
 }
